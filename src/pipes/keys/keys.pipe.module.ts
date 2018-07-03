4b32c9c1893aa2945589c87ba94af4c64abc353c
Max ang <maxsayr5@gmail.com>
2018-07-03 11:58:39 +0700

    add keys pipe

---
+++ src/pipes/keys/keys.pipe.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { KeysPipe } from "./keys.pipe";
+
+@NgModule({
+  declarations: [KeysPipe],
+  exports: [KeysPipe]
+})
+export class KeysPipeModule {}
